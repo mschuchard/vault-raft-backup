5dd51a4019463ca09232025bcc1d8caa0e4674f5
Matt Schuchard <grapheneowns@gmail.com>
2024-04-19 12:10:57 -0400

    improve aws unit tests

--- aws/s3_test.go
+++ aws/s3_test.go
@@ -6,21 +6,28 @@ import (
 	"testing"
 )
 
+const (
+	prefix = "my_prefix"
+	bucket = "my_bucket"
+)
+
+var expectedConfig awsConfig = awsConfig{s3Bucket: "my_bucket", s3Prefix: "my_prefix"}
+
 func TestNewAWSConfig(test *testing.T) {
-	os.Setenv("S3_PREFIX", "my_prefix")
+	os.Setenv("S3_PREFIX", prefix)
 	_, err := NewAWSConfig()
 	if err == nil || err.Error() != "empty s3 bucket input setting" {
 		test.Errorf("expected error: empty s3 bucket input setting, actual %v", err)
 	}
 
-	os.Setenv("S3_BUCKET", "my_bucket")
-	awsConfig, err := NewAWSConfig()
+	os.Setenv("S3_BUCKET", bucket)
+	config, err := NewAWSConfig()
 	if err != nil {
 		test.Errorf("constructor unexpectedly errored with %v", err)
 	}
-	if awsConfig.s3Bucket != "my_bucket" || awsConfig.s3Prefix != "my_prefix" {
-		test.Errorf("expected bucket value: my_bucket, actual: %s", awsConfig.s3Bucket)
-		test.Errorf("expected prefix value: my_prefix, actual: %s", awsConfig.s3Prefix)
+	if *config != expectedConfig {
+		test.Errorf("expected aws config values: %v", expectedConfig)
+		test.Errorf("actual aws config value: %v", config)
 	}
 }
 
@@ -31,9 +38,8 @@ func TestSnapshotS3Upload(test *testing.T) {
 	if err != nil {
 		test.Error("test short-circuited because file could not be created and opened")
 	}
-	awsConfig := &awsConfig{s3Bucket: "my_bucket"}
 
-	if _, err := SnapshotS3Upload(awsConfig, "foo"); err == nil || !strings.Contains(err.Error(), "NoCredentialProviders: no valid providers in chain") {
+	if _, err := SnapshotS3Upload(&expectedConfig, "foo"); err == nil || !strings.Contains(err.Error(), "NoCredentialProviders: no valid providers in chain") {
 		test.Errorf("expected error (contains): NoCredentialProviders: no valid providers in chain, actual: %v", err)
 	}
 }
