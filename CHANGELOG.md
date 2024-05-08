9004b8987ce3bb03ddae061acc6bb7386ceb6b60
Matt Schuchard <grapheneowns@gmail.com>
2024-05-08 12:54:54 -0400

    add frontend functionality for snapshot cleanup option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 1.1.0 (Next)
+- Convert local snapshot cleanup from forced to optional.
+
 ### 1.0.1
 - Improve AWS S3 config handling.
 - Validate Vault server is unsealed.
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/mitodl/vault-raft-backup/aws"
 	"github.com/mitodl/vault-raft-backup/vault"
@@ -35,7 +37,13 @@ func main() {
 	}
 
 	// upload snapshot to aws s3
-	_, err = aws.SnapshotS3Upload(awsConfig, snapshotFile.Name(), true)
+	cleanup, err := strconv.ParseBool(os.Getenv("SNAPSHOT_CLEANUP"))
+	if err != nil {
+		log.Printf("invalid boolean value '%s' for SNAPSHOT_CLEANUP", os.Getenv("SNAPSHOT_CLEANUP"))
+		log.Fatal(err)
+	}
+
+	_, err = aws.SnapshotS3Upload(awsConfig, snapshotFile.Name(), cleanup)
 	if err != nil && err.Error() != "snapshot not found" && err.Error() != "snapshot not removed" {
 		// not an error from failed removal so error is actually fatal
 		log.Print("S3 upload failed")
