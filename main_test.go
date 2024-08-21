beb0ecab9fb602107fbb3d72888d829a4de71e17
Matt Schuchard <grapheneowns@gmail.com>
2024-08-21 15:37:02 -0400

    fix go vet warning on example name

--- main_test.go
+++ main_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/mschuchard/vault-raft-backup/util"
 )
 
-func ExampleMain() {
-	// test that main behaves as expected before snapshot as raft not supported with vault server
+func Example() {
+	// test that main behaves as expected before snapshot as raft not supported with vault dev mode server
 	os.Setenv("VAULT_TOKEN", util.VaultToken)
 	os.Setenv("S3_BUCKET", "bucket")
 
