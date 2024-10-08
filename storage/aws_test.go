710f1ccf452f0e160e03923a6ea34f6fdb132435
Matt Schuchard <grapheneowns@gmail.com>
2024-10-08 09:18:47 -0400

    update unit tests for unified config

--- storage/aws_test.go
+++ storage/aws_test.go
@@ -4,36 +4,7 @@ import (
 	"os"
 	"strings"
 	"testing"
-
-	"github.com/mschuchard/vault-raft-backup/util"
 )
-
-const (
-	bucket = "my_bucket"
-	prefix = "my_prefix"
-)
-
-var (
-	backupAWSConfig = &util.AWSConfig{S3Prefix: prefix}
-	expectedConfig  = awsConfig{s3Bucket: bucket, s3Prefix: prefix}
-)
-
-func TestNewAWSConfig(test *testing.T) {
-	_, err := newAWSConfig(backupAWSConfig)
-	if err == nil || err.Error() != "empty s3 bucket input setting" {
-		test.Errorf("expected error: empty s3 bucket input setting, actual %v", err)
-	}
-
-	backupAWSConfig.S3Bucket = bucket
-	config, err := newAWSConfig(backupAWSConfig)
-	if err != nil {
-		test.Errorf("constructor unexpectedly errored with %v", err)
-	}
-	if *config != expectedConfig {
-		test.Errorf("expected aws config values: %v", expectedConfig)
-		test.Errorf("actual aws config value: %v", *config)
-	}
-}
 
 func TestSnapshotS3Upload(test *testing.T) {
 	// test this fails at s3upload
