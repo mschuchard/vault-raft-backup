66dd61db1f2fa6124ea9263d6c48c32604361b11
Matt Schuchard <grapheneowns@gmail.com>
2024-11-13 10:12:20 -0500

    add unified storage transfer func unit test

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,6 +1,15 @@
 package storage
 
-const (
-	object = "my_bucket"
-	prefix = "my_prefix"
+import (
+	"testing"
+
+	"github.com/mschuchard/vault-raft-backup/util"
 )
+
+const object = "my_bucket"
+
+func TestStorageTransfer(test *testing.T) {
+	if err := StorageTransfer(&util.CloudConfig{}, "/foo", true); err == nil {
+		test.Error("did not return error as expected for nonexistent snapshot file")
+	}
+}
