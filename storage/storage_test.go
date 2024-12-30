3826795fe581a6c8280927f4dfe02c66fcb54c74
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2024-12-30 09:36:26 -0500

    add invalid cloud platform unit test

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -10,4 +10,9 @@ func TestStorageTransfer(test *testing.T) {
 	if err := StorageTransfer(&util.CloudConfig{}, "/foo", true); err == nil {
 		test.Error("did not return error as expected for nonexistent snapshot file")
 	}
+
+	if err := StorageTransfer(&util.CloudConfig{Platform: "doesnotexist"}, "../.gitignore", true); err == nil || err.Error() != "invalid cloud platform" {
+		test.Error("unexpected or no error returned")
+		test.Errorf("expected: invalid cloud platform, actual: %s", err)
+	}
 }
