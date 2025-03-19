0aef3063d95e1c27beabe8284de448159c073f59
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-03-19 10:49:45 -0400

    add enum package for pseudo-enum support

---
+++ enum/enum.go
@@ -0,0 +1,41 @@
+package enum
+
+import (
+	"errors"
+	"log"
+)
+
+// platform pseudo-enum
+type Platform string
+
+const (
+	AWS   Platform = "aws"
+	GCP   Platform = "gcp"
+	LOCAL Platform = "local"
+)
+
+// platform type conversion
+func (Platform) from(s Platform) (Platform, error) {
+	if s != AWS && s != GCP && s != LOCAL {
+		log.Printf("string %s could not be converted to Platform enum", s)
+		return "", errors.New("invalid platform enum")
+	}
+	return Platform(s), nil
+}
+
+// authengine pseudo-enum
+type AuthEngine string
+
+const (
+	AWSIAM     AuthEngine = "aws"
+	VaultToken AuthEngine = "token"
+)
+
+// authengine type conversion
+func (AuthEngine) from(s AuthEngine) (AuthEngine, error) {
+	if s != AWSIAM && s != VaultToken {
+		log.Printf("string %s could not be converted to AuthEngine enum", s)
+		return "", errors.New("invalid authengine enum")
+	}
+	return AuthEngine(s), nil
+}
---
+++ enum/enum_test.go
@@ -0,0 +1,11 @@
+package enum
+
+import "testing"
+
+func TestPlatformFromString(test *testing.T) {
+
+}
+
+func TestAuthEngineFromString(test *testing.T) {
+
+}
