9a8df53f2d992377aad47264bf5527d526ad744b
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-04-15 17:13:55 -0400

    update snapshot restoration unit test for new bootstrap

--- vault/snapshot_test.go
+++ vault/snapshot_test.go
@@ -2,7 +2,6 @@ package vault
 
 import (
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/mschuchard/vault-raft-backup/util"
@@ -26,12 +25,17 @@ func TestVaultRaftSnapshotCreate(test *testing.T) {
 }
 
 func TestVaultRaftSnapshotRestore(test *testing.T) {
-	err := VaultRaftSnapshotRestore(util.VaultClient, "snapshot_test.go")
-	if err == nil || !strings.Contains(err.Error(), "POST http://127.0.0.1:8200/v1/sys/storage/raft/snapshot") {
-		test.Errorf("expected error (contains): POST http://127.0.0.1:8200/v1/sys/storage/raft/snapshot, actual: %v", err)
+	if err := VaultRaftSnapshotCreate(util.VaultClient, "vault.bak"); err != nil {
+		test.Error("vault raft snapshot creation for snapshot restoration test failed")
+	}
+	if err := VaultRaftSnapshotRestore(util.VaultClient, "vault.bak"); err != nil {
+		test.Error("vault raft snapshot restoration failed")
+		test.Error(err)
 	}
 
-	if err = VaultRaftSnapshotRestore(util.VaultClient, "/foo/vault.bak"); err == nil || err.Error() != "open /foo/vault.bak: no such file or directory" {
+	os.Remove("./vault.bak")
+
+	if err := VaultRaftSnapshotRestore(util.VaultClient, "/foo/vault.bak"); err == nil || err.Error() != "open /foo/vault.bak: no such file or directory" {
 		test.Errorf("expected error (contains): open /foo/vault.bak: no such file or directory, actual: %v", err)
 	}
 }
