7c81bf9954ec93931772ca440fd308c72e0d4905
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-06-18 13:51:51 -0400

    remove supported releases combos and update accept test pre-checks

--- main_test.go
+++ main_test.go
@@ -16,12 +16,20 @@ func Example() {
 	if err != nil {
 		log.Fatalf("failed to create vault client for validating server: %s", err)
 	}
+	// ensure vault server is healthy after snapshot restoration during unit tests
 	for i := range 16 {
-		if _, err := client.Sys().SealStatus(); err == nil {
+		// cluster healthy and unsealed?
+		if health, err := client.Sys().Health(); err == nil && !health.Sealed {
 			break
 		} else if i == 15 {
+			// check if error
+			if err != nil {
+				log.Print(err)
+			}
+			// for some reason the server never recovered
 			log.Fatalf("vault server was not available after fifteen seconds")
 		}
+		// otherwise wait and try again
 		time.Sleep(1 * time.Second)
 	}
 
